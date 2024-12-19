101f2b32efbf005927a32323351e316ac171b8d3
LingarajuR <lingarajurnie@gmail.com>
2024-12-19 15:10:24 +0530

    Update CowsandbullssApplicationTests.java

--- src/test/java/com/nsp/cowsandbullss/CowsandbullssApplicationTests.java
+++ src/test/java/com/nsp/cowsandbullss/CowsandbullssApplicationTests.java
@@ -3,14 +3,12 @@ package com.nsp.cowsandbullss;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = {CowsandbullssApplication.class})
 public class CowsandbullssApplicationTests {
